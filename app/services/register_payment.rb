912e2b52b6e8fe09c33fccb62a399225121491d5
lougansjs <lougans.lougans99@gmail.com>
2026-01-12 11:47:10 -0300

    chore: adds validation in case the payment has already been made.

--- app/services/register_payment.rb
+++ app/services/register_payment.rb
@@ -8,6 +8,8 @@ module Services
       record = @repository.find_by_id(id)
       raise StandardError, "Parking record not found" unless record
 
+      raise StandardError, "Parking record already paid" if record.paid
+
       record.paid = true
       record.payment_time = Time.now.utc
       @repository.update(record)
